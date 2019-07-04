d962ce389438e7cc1551612c079b738815025363
BitSandDev <50877584+BitSandDev@users.noreply.github.com>
2019-07-04 12:01:45 +0100

    Delete Tracey.bat

--- TraceyUploader/TraceyUploader/bin/Debug/Tracey.bat
+++
@@ -1,1 +0,0 @@
-"C:\Users\anthony.russell\Google Drive\Projects\Tracey\C#\UpLoader\TraceyUploader\TraceyUploader\bin\Debug/esptool.exe" -vv -cd nodemcu -cb 115200 -cp COM10 -ca 0x00000 -cf "C:\Users\anthony.russell\Google Drive\Projects\Tracey\C#\UpLoader\TraceyUploader\TraceyUploader\bin\Debug/Tracey.bin"
