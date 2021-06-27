7f2672d1601ae98067b7eea3a0a79f260fdb2fd8
BitSandDev <50877584+BitSandDev@users.noreply.github.com>
2021-06-27 16:56:24 +0100

    Update Form1.cs

--- TraceyUploader/TraceyUploader/Form1.cs
+++ TraceyUploader/TraceyUploader/Form1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System;
 using System.Windows.Forms;
 using System.IO.Ports;
 using System.IO;
@@ -13,9 +7,6 @@ using System.Diagnostics;
 
 namespace TraceyUploader
 {
-
-
-
 
     public partial class Form1 : Form
     {
